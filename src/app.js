4a08bec0f48a23f0d4fc56e2187599fff0c940d1
Plesca <aplesca@techteam.com>
2020-07-25 19:40:14 +0300

    corrections

--- src/app.js
+++ src/app.js
@@ -1,37 +1,22 @@
 const app = angular.module('myApp', []);
 
-app.controller('MainController', function ($http) {
-  this.getLink = function (category) {
-    return `https://api.themoviedb.org/3/movie/${category}?api_key=${tmdbKey}`;
-  };
+app.controller('MainController', function (movieService) {
+  movieService.getData('now_playing')
+    .then((result) => this.nowPlaying = result);
 
-  $http.get(this.getLink('now_playing'))
-    .then((response) => {
-      this.nowPlaying = response.data.results;
-    });
+  movieService.getData('popular')
+    .then((result) => this.latest = result);
 
-  // this.nowPlaying = getData('now_playing');
-
-  $http.get(this.getLink('popular'))
-    .then((response) => {
-      this.latest = response.data.results;
-    });
-
-  $http.get(this.getLink('upcoming'))
-    .then((response) => {
-      this.upcoming = response.data.results;
-    });
+  movieService.getData('upcoming')
+    .then((result) => this.upcoming = result);
 
   this.getImage = function (movie) {
     return imageURL + movie.poster_path;
   };
 });
 
-app.factory('getData', function ($http, category) {
-  $http.get(this.getLink(category))
-    .then((response) => {
-      toReturn = response.data.results;
-    });
+// app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+//   $locationProvider.hashPrefix('!');
 
-  return toReturn;
-});
+//   $routeProvider.otherwise({ redirectTo: '/list' });
+// }]);
--- src/list.js
+++
@@ -1,8 +0,0 @@
-(function (angular) {
-  angular.module('myApp').component('list', {
-    templateUrl: '<h2>Now Playing</h2>',
-    bindings: {
-      hero: '=',
-    },
-  });
-}(window.angular));
