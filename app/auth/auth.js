6b03669cbe1b5abdc2893dceacdaccba53f93d01
Plesca <aplesca@techteam.com>
2020-07-29 15:09:37 +0300

    fix: working

--- app/auth/auth.js
+++ app/auth/auth.js
@@ -1,18 +1,17 @@
 angular.module('app.auth', ['ngRoute'])
   .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/1', {
+    $routeProvider.when('/', {
       templateUrl: 'auth/auth.html',
       controller: 'authCtrl',
     });
   }])
   .controller('authCtrl', (authService) => {
-    // $ctrl = this;
     this.$onInit = function () {
       authService.getToken()
         .then(
           (result) => this.logInLink = `https://www.themoviedb.org/authenticate/${result.request_token}?redirect_to=http://localhost:8000/#!/approved`,
         );
     };
-    // console.log(this.Token);
+ 
     // this.logInLink = `https://www.themoviedb.org/authenticate/${this.authToken}?redirect_to=http://localhost:8000/#!/approved`;
   });
