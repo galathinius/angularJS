5eb8d87e7428a903b822184aeeec3bda04004cc4
Plesca <aplesca@techteam.com>
2020-08-03 14:52:38 +0300

    fix: location provider search

--- app/MovieLists/MovieLists.js
+++ app/MovieLists/MovieLists.js
@@ -1,17 +1,18 @@
 angular
   .module("app.movieLists", ["ngRoute"])
 
-  .controller("movieListsCtrl", function (authService) {
-    const url = window.location.href;
-    const regToken = /request_token=(.*)&approved=true/;
-    const newToken = url.match(regToken);
+  .controller("movieListsCtrl", function (authService, $location) {
+    // const url = window.location.href;
+    // const regToken = /request_token=(.*)&approved=true/;
+    // const newToken = url.match(regToken);
+    // if (newToken) {
+    //   console.log("extract token", newToken[1]);
+    //   authService.getSessionId(newToken[1]).then((res) => {
+    //     localStorage.setItem("session_id", res.data.session_id);
+    //     return res.data.session_id;
+    //   });
+    // }
 
-    if (newToken) {
-      console.log("extract token", newToken[1]);
-
-      authService.getSessionId(newToken[1]).then((res) => {
-        localStorage.setItem("session_id", res.data.session_id);
-        return res.data.session_id;
-      });
-    }
+    const qs = $location.search();
+    console.log(qs);
   });
--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
-angular.module("app", [
-  "app.movieLists",
-  "ngRoute",
-  "app.auth",
-  "app.listCreate",
-]);
+angular
+  .module("app", ["app.movieLists", "ngRoute", "app.auth", "app.listCreate"])
+  .config(function ($locationProvider) {
+    $locationProvider.html5Mode(true);
+  });
