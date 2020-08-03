a1fd8152d94cd611a30845a50a2a57631c366af0
Plesca <aplesca@techteam.com>
2020-08-03 14:07:13 +0300

    feat: create list with form

--- app/listCreate/listCreate.js
+++ app/listCreate/listCreate.js
@@ -1,9 +1,11 @@
 angular
   .module("app.listCreate", ["ngRoute"])
   .controller("createCtrl", function (listsService) {
-    listsService.createList("brand new list").then((res) => {
-      localStorage.setItem("list_id", res.data.list_id);
-      console.log(`status_message : ${res.data.status_message}`);
-      return res.data.session_id;
-    });
+    this.update = function (list) {
+      console.log(list);
+      listsService.createList(list.name, list.description).then((res) => {
+        localStorage.setItem("list_id", res.data.list_id);
+        console.log(`status_message : ${res.data.status_message}`);
+      });
+    };
   });
--- app/services/listsService.js
+++ app/services/listsService.js
@@ -2,10 +2,10 @@ const sessionId = localStorage.getItem("session_id");
 const listCreateUrl = `https://api.themoviedb.org/3/list?api_key=${tmdbKey}&session_id=${sessionId}`;
 
 angular.module("app").factory("listsService", ($http) => ({
-  createList(listName) {
+  createList(listName, listDescription) {
     requestBody = {
       name: listName,
-      description: "Just an awesome list dawg.",
+      description: listDescription,
       language: "en",
     };
     return $http.post(listCreateUrl, requestBody);
