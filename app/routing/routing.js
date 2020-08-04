4941290077e9f7cc3570dc3f0cd4c0baf8f044f8
Plesca <aplesca@techteam.com>
2020-08-04 19:53:36 +0300

    refactor

--- app/routing/routing.js
+++ app/routing/routing.js
@@ -1,16 +1,16 @@
 angular.module("app").config(($routeProvider) => {
   $routeProvider
     .when("/", {
-      templateUrl: "components/movieLists/movieLists.html",
+      templateUrl: "components/movieLists/movieLists.view.html",
       controller: "movieListsCtrl",
     })
     .when("/logIn", {
-      templateUrl: "components/auth/auth.html",
+      templateUrl: "components/auth/auth.view.html",
       controller: "authCtrl",
       controllerAs: "$ctrl",
     })
     .when("/create", {
-      templateUrl: "components/listCreate/listCreate.html",
+      templateUrl: "components/listCreate/listCreate.view.html",
       controller: "createCtrl",
       controllerAs: "$ctrl",
     })
